ed0e187a2742a69a76687ac3d8f13d334f7ac49a
Aviral Singh <aviralsingh3501@gmail.com>
2024-08-13 15:31:06 +0530

    rest

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,48 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Welcome to cloudfare workers');
-	},
-} satisfies ExportedHandler<Env>;
+
+// export default {
+// 	async fetch(request, env, ctx): Promise<Response> {
+// 		return new Response('Welcome to cloudfare');
+// 	},
+// } satisfies ExportedHandler<Env>;
+
+import OpenAI from "openai";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+
+type Bindings={
+	OPEN_AI_KEY:OpenAI;
+	AI:Ai,
+}
+
+const app = new Hono<{Bindings:Bindings}>();
+
+// cors
+app.use('/*',cors({
+	origin:'*', //Allow requests from nextjs app
+	allowHeaders:['X-Custom-Header','Upgrade-Insecure-Requests','Content-Type'],//ADD THE CONETENT TYPE TO THE HEADERS TO FIX CORS
+	allowMethods:['POST','GET','PUT','OPTIONS'],
+	exposeHeaders:['Content-length','X-Kuma-Revision'],
+	maxAge:600,
+	credentials:true,
+}))
+
+// creating thw post endopoint
+app.post('/translate',async(c)=>{
+	const {documentData,targetLang} = await c.req.json();
+
+	// generate a summary for the document
+	const summaryResponse = await c.env.AI.run('@cf/facebook/bart-large-cnn',{
+		input_text:documentData,
+		max_length:1000,
+	});
+
+	// translate the summary into another language
+	const response = await c.env.AI.run('@cf/meta/m2m100-1.2b',{
+		text:summaryResponse.summary,
+		source_lang:'english',
+		target_lang:targetLang,
+	});
+
+	return new Response(JSON.stringify(response));
+})
